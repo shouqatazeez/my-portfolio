18cb54735f89a385381c04bb82f816a6156e71dc
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-08-01 01:23:35 +0530

    add the navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,121 @@
+import { Button } from "@/components/ui/button";
+import { FileText } from "lucide-react";
+
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import Devlogo from "./Devlogo";
+import { Text } from "lucide-react";
+
+// Navigation links array to be used in both desktop and mobile menus
+const navigationLinks = [
+  { href: "#", label: "Home", active: true },
+  { href: "#", label: "Features" },
+  { href: "#", label: "Pricing" },
+  { href: "#", label: "About" },
+];
+
+function Navbar() {
+  return (
+    <header className="border-b px-4 md:px-6 shadow-sm ">
+      <div className="flex h-16 items-center justify-between gap-4">
+        {/* Left side: logo and mobile toggle */}
+        <div className="flex items-center gap-2">
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button
+                className="group size-8 md:hidden"
+                variant="ghost"
+                size="icon"
+              >
+                {/* Mobile icon */}
+                <svg
+                  className="pointer-events-none"
+                  width={16}
+                  height={16}
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M4 12L20 12"
+                    className="origin-center -translate-y-[7px] transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)] group-aria-expanded:translate-x-0 group-aria-expanded:translate-y-0 group-aria-expanded:rotate-[315deg]"
+                  />
+                  <path
+                    d="M4 12H20"
+                    className="origin-center transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.8)] group-aria-expanded:rotate-45"
+                  />
+                  <path
+                    d="M4 12H20"
+                    className="origin-center translate-y-[7px] transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)] group-aria-expanded:translate-y-0 group-aria-expanded:rotate-[135deg]"
+                  />
+                </svg>
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent align="start" className="w-36 p-1 md:hidden">
+              <NavigationMenu className="max-w-none *:w-full">
+                <NavigationMenuList className="flex-col items-start gap-0 md:gap-2">
+                  {navigationLinks.map((link, index) => (
+                    <NavigationMenuItem key={index} className="w-full">
+                      <NavigationMenuLink
+                        href={link.href}
+                        className="py-1.5"
+                        active={link.active}
+                      >
+                        {link.label}
+                      </NavigationMenuLink>
+                    </NavigationMenuItem>
+                  ))}
+                </NavigationMenuList>
+              </NavigationMenu>
+            </PopoverContent>
+          </Popover>
+
+          {/* Logo only */}
+          <a href="#" className="text-primary hover:text-primary/90">
+            <Devlogo />
+          </a>
+        </div>
+
+        {/* Right side: nav links + resume */}
+        <div className="flex items-center gap-6">
+          <NavigationMenu className="max-md:hidden">
+            <NavigationMenuList className="gap-2 ">
+              {navigationLinks.map((link, index) => (
+                <NavigationMenuItem key={index}>
+                  <NavigationMenuLink
+                    active={link.active}
+                    href={link.href}
+                    className="text-muted-foreground hover:text-primary py-1.5 font-medium"
+                  >
+                    {link.label}
+                  </NavigationMenuLink>
+                </NavigationMenuItem>
+              ))}
+            </NavigationMenuList>
+          </NavigationMenu>
+
+          <Button asChild size="sm" className="text-sm">
+            <a href="/resume.pdf" download className="flex items-center gap-1">
+              <FileText className="size-4" />
+              <span>Resume</span>
+            </a>
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
+}
+export default Navbar;
