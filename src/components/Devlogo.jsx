58b7b6ddfd33b7c761761f9bc7453601010c85e3
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-08-01 01:23:04 +0530

    add devlogo file

---
+++ src/components/Devlogo.jsx
@@ -0,0 +1,11 @@
+const Devlogo = () => {
+  return (
+    <div className="flex items-center gap-2 select-none">
+      <span className="text-xl font-bold tracking-tight text-black hover:text-shadow-black transition-all duration-300 font-mono">
+        &lt;/Shouqat&gt;
+      </span>
+    </div>
+  );
+};
+
+export default Devlogo;
