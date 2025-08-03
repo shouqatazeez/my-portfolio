abd4787752ac7b1dd50f630a2d08d1b7fe8df653
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-08-03 22:23:18 +0530

    add about component in app

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import Navbar from "./components/Navbar";
 import Hero from "./pages/Hero";
+import About from "./pages/About";
 
 const App = () => {
   return (
     <div className="">
       <Navbar />
       <Hero />
+      <About />
     </div>
   );
 };
