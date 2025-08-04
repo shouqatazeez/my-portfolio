188e475c9469737d5678d708d8bfc790af03bd3b
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-08-04 21:36:11 +0530

    changed the bg color in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import React from "react";
-
 import Navbar from "./components/Navbar";
 import Hero from "./pages/Hero";
 import About from "./pages/About";
 
 const App = () => {
   return (
-    <div className="">
+    <div>
       <Navbar />
-      <Hero />
-      <About />
+      <div className="relative min-h-screen w-full bg-white">
+        <div
+          className="absolute h-full w-full bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] 
+                     [background-size:16px_16px] 
+                     [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)]"
+        />
+
+        <div className="relative z-10">
+          <Hero />
+          <About />
+        </div>
+      </div>
     </div>
   );
 };
