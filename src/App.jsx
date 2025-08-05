49982387dfcba358c7616b3c09b426c4138688d4
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-08-06 00:40:47 +0530

    add some minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
 import Navbar from "./components/Navbar";
 import Hero from "./pages/Hero";
 import About from "./pages/About";
+import Projects from "./pages/Projects";
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <div className="relative min-h-screen w-full bg-white pt-16">
+      <div className="relative min-h-screen w-full bg-white pt-16 scroll-smooth">
         <div
           className="absolute h-full w-full bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] 
                      [background-size:16px_16px] 
                      [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)]"
         />
-
         <div className="relative z-10">
-          <Hero />
-          <About />
+          <section id="home">
+            <Hero />
+          </section>
+          <section id="about">
+            <About />
+          </section>
+          <section id="projects">
+            <Projects />
+          </section>
         </div>
       </div>
     </div>
