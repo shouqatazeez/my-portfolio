d192767f5a42eb3d606a0f33ab63c53e87126ba0
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-08-06 21:50:15 +0530

    some modifications in firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,3 @@
-
-// import { initializeApp } from "firebase/app";
-// import { getFirestore } from "firebase/firestore";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyD2SD3K2_ffCEAaJaxFLwnNl1VgVzVWtZ0",
-//   authDomain: "my-portfolio-contact-b0ca8.firebaseapp.com",
-//   projectId: "my-portfolio-contact-b0ca8",
-//   storageBucket: "my-portfolio-contact-b0ca8.firebasestorage.app",
-//   messagingSenderId: "226772195041",
-//   appId: "1:226772195041:web:677beed6369dd85d2d89a0",
-// };
-
-// const app = initializeApp(firebaseConfig);
-
-// const db = getFirestore(app);
-
-// export default db;
-
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
